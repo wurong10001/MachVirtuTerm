c6e0f0c030f38dae342241a1fc07fd5ab7c05f9f
wurong10001 <qingniao10001@outlook.com>
2026-08-30 19:49:16 +0800

    chore: remove non-source files (build artifacts, logs, dev scripts, IDE config)

--- check_autoconf.ps1
+++
@@ -1,9 +0,0 @@
-$clang = 'C:\Users\Administrator\Desktop\android\sdk\ndk\28.2.13676358\toolchains\llvm\prebuilt\windows-x86_64\bin\clang.exe'
-$src   = 'C:\Users\Administrator\Desktop\Projects\AndroidStudioProjects\Mos\app\src\main\cpp\kernel\src\mach_kernel\hp_pa\HP700\autoconf.c'
-$inc1  = 'C:\Users\Administrator\Desktop\Projects\AndroidStudioProjects\Mos\app\src\main\cpp\android_compat'
-$inc2  = 'C:\Users\Administrator\Desktop\Projects\AndroidStudioProjects\Mos\app\src\main\cpp\kernel\src\mach_kernel'
-$log   = 'C:\Users\Administrator\Desktop\Projects\AndroidStudioProjects\Mos\autoconf_check.txt'
-$out = & $clang -target aarch64-linux-android28 -fsyntax-only -std=gnu89 -DMACH_KERNEL -Wno-implicit-function-declaration -Wno-int-conversion -Wno-implicit-int -Wno-pointer-to-int-cast -Wno-macro-redefined -Wno-incompatible-library-redeclaration -I $inc1 -I $inc2 $src 2>&1
-$out | Set-Content -Path $log -Encoding utf8
-Write-Output ("EXIT=" + $LASTEXITCODE)
-Write-Output ("LINES=" + $out.Count)
